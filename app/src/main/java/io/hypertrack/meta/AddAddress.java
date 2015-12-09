036696b11859d6e5c568ceb6064b7484003a5383
Suhas <suhas_sm@gmail.com>
2015-12-09 15:02:25 +0530

    adds form for adding custom address - not impemented yet

---
+++ app/src/main/java/io/hypertrack/meta/AddAddress.java
@@ -0,0 +1,13 @@
+package io.hypertrack.meta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_address);
+    }
+}
