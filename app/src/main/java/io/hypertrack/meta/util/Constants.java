21867310af11ad172bf3dfd4cbd3cf23414dc369
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2016-06-25 16:54:11 +0530

    Cleanup - removes volley classes

--- app/src/main/java/io/hypertrack/meta/util/Constants.java
+++ app/src/main/java/io/hypertrack/meta/util/Constants.java
@@ -1,12 +1,5 @@
 package io.hypertrack.meta.util;
 
-import android.util.Log;
-
-import io.hypertrack.meta.BuildConfig;
-import io.hypertrack.meta.network.HTCustomGetRequest;
-import io.hypertrack.meta.network.HTCustomPostRequest;
-import io.hypertrack.meta.network.HTMultipartRequest;
-
 
 /**
  * Created by suhas on 12/11/15.
@@ -19,5 +12,4 @@ public class Constants {
     public static final String USER_PROFILE_PIC = "user_profile_pic";
 
     public static final String USER_PROFILE_PIC_ENCODED = "user_profile_pic_encoded";
-    public static final String GCM_REGISTRATION_TOKEN = "gcm_registration_token";
 }
