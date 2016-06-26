30cae18881fdeb5d4909bdf4ebf1450c7066120e
Piyush <piyush@Piyushs-Mac.local>
2016-06-26 13:23:00 +0530

    Adds Swipe to Refresh in UserProfile, Adds Null checks in MetaPlace constructor

--- app/src/main/java/io/hypertrack/meta/view/UserProfile.java
+++ app/src/main/java/io/hypertrack/meta/view/UserProfile.java
@@ -2,15 +2,17 @@ package io.hypertrack.meta.view;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ScrollView;
 
 import io.hypertrack.meta.R;
 import io.hypertrack.meta.adapter.FavoritePlacesAdapter;
@@ -25,7 +27,9 @@ public class UserProfile extends AppCompatActivity implements FavoritePlaceOnCli
 
     private final String TAG = UserProfile.class.getSimpleName();
 
+    private ScrollView mScrollView;
     private RecyclerView mRecyclerView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     private FavoritePlacesAdapter favoritePlacesAdapter;
     private ProgressDialog mProgressDialog;
 
@@ -36,9 +40,22 @@ public class UserProfile extends AppCompatActivity implements FavoritePlaceOnCli
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
         this.updateTitle();
 
+        mScrollView = (ScrollView) findViewById(R.id.scrollView);
+        mScrollView.smoothScrollTo(0, 0);
+
         mRecyclerView = (RecyclerView) findViewById(R.id.user_profile_saved_places);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        mSwipeRefreshLayout.setColorSchemeColors(Color.GRAY);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                UserProfile.this.refreshFavorites();
+            }
+        });
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setAutoMeasureEnabled(true);
@@ -57,10 +74,6 @@ public class UserProfile extends AppCompatActivity implements FavoritePlaceOnCli
         mRecyclerView.setAdapter(favoritePlacesAdapter);
     }
 
-    public void refreshButtonClicked(MenuItem menuItem) {
-        this.refreshFavorites();
-    }
-
     public void onEditAccountClick(View view) {
         Intent editProfileIntent = new Intent(this, EditProfile.class);
         startActivityForResult(editProfileIntent, EditProfile.EDIT_PROFILE_RESULT_CODE, null);
@@ -72,6 +85,7 @@ public class UserProfile extends AppCompatActivity implements FavoritePlaceOnCli
 //    }
 
     private void refreshFavorites() {
+
         mProgressDialog = new ProgressDialog(this);
         mProgressDialog.setMessage("Refresh favorite places");
         mProgressDialog.setCancelable(false);
@@ -82,6 +96,9 @@ public class UserProfile extends AppCompatActivity implements FavoritePlaceOnCli
             public void OnSuccess() {
                 mProgressDialog.dismiss();
                 updateFavoritesAdapter();
+
+                // Hide the Swipe Refresh Loader
+                mSwipeRefreshLayout.setRefreshing(false);
             }
 
             @Override
@@ -89,12 +106,6 @@ public class UserProfile extends AppCompatActivity implements FavoritePlaceOnCli
                 mProgressDialog.dismiss();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_user_profile, menu);
-        return super.onCreateOptionsMenu(menu);
     }
 
     private void updateTitle() {
@@ -194,5 +205,15 @@ public class UserProfile extends AppCompatActivity implements FavoritePlaceOnCli
         } else if (requestCode == EditProfile.EDIT_PROFILE_RESULT_CODE) {
             this.updateTitle();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //back button inside toolbar
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        } else
+            return false;
     }
 }
