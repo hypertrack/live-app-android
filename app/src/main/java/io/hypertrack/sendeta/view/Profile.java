abb9dceeef5f423424ef269feac537700462803a
Piyush <piyush@Piyushs-Mac.local>
2016-07-11 14:35:46 +0530

    Adds ProfileImage Click on successful permission grant

--- app/src/main/java/io/hypertrack/sendeta/view/Profile.java
+++ app/src/main/java/io/hypertrack/sendeta/view/Profile.java
@@ -279,6 +279,7 @@ public class Profile extends BaseActivity implements ProfileView {
         switch (requestCode) {
             case PermissionUtils.REQUEST_CODE_PERMISSION_READ_EXTERNAL_STORAGE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    onProfileImageViewClicked(null);
                     //Toast.makeText(this,"Permission Granted, Now you can access location data.",Toast.LENGTH_SHORT).show();
                 }
                 break;
