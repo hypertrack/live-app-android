b728c21814d75274a2466c90c6e925698d332e7c
Piyush Gupta <piyush@hypertrack.io>
2016-11-09 21:47:44 +0530

    Removes BootReceiverService

--- app/src/main/java/io/hypertrack/sendeta/util/BootReceiver.java
+++ app/src/main/java/io/hypertrack/sendeta/util/BootReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 
 import io.hypertrack.lib.common.util.HTLog;
+import io.hypertrack.sendeta.store.TaskManager;
 
 /**
  * Created by piyush on 16/07/16.
@@ -17,7 +18,10 @@ public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         HTLog.i(TAG, "SendETA Boot receiver onReceive");
 
-        Intent onBootServiceIntent = new Intent(context, BootReceiverService.class);
-        context.startService(onBootServiceIntent);
+        if (SharedPreferenceManager.getGeofencingRequest() != null) {
+            // Add Geofencing Request
+            TaskManager.getSharedManager(context).setGeofencingRequest(SharedPreferenceManager.getGeofencingRequest());
+            TaskManager.getSharedManager(context).addGeofencingRequest();
+        }
     }
 }
