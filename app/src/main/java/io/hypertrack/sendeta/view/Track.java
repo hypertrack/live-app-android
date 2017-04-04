12bbfa8cd30b423bdaf599671340782538372989
Aman Jain <aman@hypertrack.io>
2017-04-04 13:41:28 +0530

    home screen will show when back press on tracking screen

--- app/src/main/java/io/hypertrack/sendeta/view/Track.java
+++ app/src/main/java/io/hypertrack/sendeta/view/Track.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.TaskStackBuilder;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -146,6 +147,12 @@ public class Track extends BaseActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        TaskStackBuilder.create(this)
+                .addNextIntentWithParentStack(new Intent(this, Home.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                .startActivities();
+        finish();
+
     }
 
     private class MyMapAdapter extends HyperTrackMapAdapter {
