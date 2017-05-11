7100fbc3d11a6f8dad6e5e0a3b861f7c2d7cfabe
Piyush Gupta <piyush@hypertrack.io>
2017-05-12 00:44:21 +0530

    Upgrades gcm to fcm integration

--- app/src/main/java/io/hypertrack/sendeta/service/MetaFCMListenerService.java
+++ app/src/main/java/io/hypertrack/sendeta/service/MetaFCMListenerService.java
@@ -23,12 +23,12 @@ SOFTWARE.
 */
 package io.hypertrack.sendeta.service;
 
-import com.hypertrack.lib.HyperTrackGcmListenerService;
+import com.hypertrack.lib.HyperTrackFirebaseMessagingService;
 
 /**
  * Created by piyush on 27/07/16.
  */
-public class MetaGCMListenerService extends HyperTrackGcmListenerService {
+public class MetaFCMListenerService extends HyperTrackFirebaseMessagingService {
     // No need to implement/override any method for HyperTrack SDK's Gcm/Fcm integration
     // In case onMessageReceived method is overridden, super.onMessageReceived() method should be called.
 }
