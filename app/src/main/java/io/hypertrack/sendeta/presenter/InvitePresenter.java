2dec39b510883cebcba272010bfe4a90f9835e9c
Aman <aman@hypertrack.io>
2017-07-20 19:58:09 +0530

    added user invite flow

---
+++ app/src/main/java/io/hypertrack/sendeta/presenter/InvitePresenter.java
@@ -0,0 +1,54 @@
+package io.hypertrack.sendeta.presenter;
+
+import android.util.Log;
+
+import com.hypertrack.lib.models.User;
+
+import io.hypertrack.sendeta.model.AcceptInviteModel;
+import io.hypertrack.sendeta.network.retrofit.HyperTrackService;
+import io.hypertrack.sendeta.network.retrofit.HyperTrackServiceGenerator;
+import io.hypertrack.sendeta.view.InviteView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by Aman on 19/07/17.
+ */
+
+public class InvitePresenter implements IInvitePresenter<InviteView> {
+    private static final String TAG = InvitePresenter.class.getSimpleName();
+    private InviteView inviteView;
+
+    @Override
+    public void attachView(InviteView view) {
+        inviteView = view;
+    }
+
+    @Override
+    public void detachView() {
+        inviteView = null;
+    }
+
+    @Override
+    public void acceptInvite(String userID, String accountID) {
+        HyperTrackService getPlacelineService = HyperTrackServiceGenerator.createService(HyperTrackService.class);
+        Call<User> call = getPlacelineService.acceptInvite(userID, new AcceptInviteModel(accountID));
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if(inviteView!=null)
+                    inviteView.inviteAccepted();
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.getMessage());
+                t.printStackTrace();
+                if(inviteView!=null)
+                    inviteView.showError();
+
+            }
+        });
+    }
+}
