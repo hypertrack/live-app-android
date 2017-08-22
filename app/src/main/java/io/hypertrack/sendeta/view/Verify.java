195dac9f535a8f8f845b1986ed630c06c08b75d8
Aman <aman@hypertrack.io>
2017-08-22 15:30:00 +0530

    open keyboard in verify screen

--- app/src/main/java/io/hypertrack/sendeta/view/Verify.java
+++ app/src/main/java/io/hypertrack/sendeta/view/Verify.java
@@ -28,6 +28,7 @@ import io.hypertrack.sendeta.presenter.IVerifyPresenter;
 import io.hypertrack.sendeta.presenter.VerifyPresenter;
 import io.hypertrack.sendeta.store.SharedPreferenceManager;
 import io.hypertrack.sendeta.util.CrashlyticsWrapper;
+import io.hypertrack.sendeta.util.Utils;
 
 /**
  * Created by Aman on 19/07/17.
@@ -99,6 +100,7 @@ public class Verify extends BaseActivity implements VerifyView {
         });
         otpEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(4)});
         otpEditText.requestFocus();
+        Utils.showKeyboard(this, otpEditText);
     }
 
     @Override
