fdaa0d8dedb02e6873813a3fc6c5b7ec42456bd0
Denys <quux@ukr.net>
2020-03-07 23:49:42 -0800

    Another test case added.

--- app/src/test/java/com/hypertrack/live/models/TripModelTest.java
+++ app/src/test/java/com/hypertrack/live/models/TripModelTest.java
@@ -1,7 +1,9 @@
 package com.hypertrack.live.models;
 
 import android.service.autofill.RegexValidator;
+import android.util.Log;
 
+import com.hypertrack.sdk.views.dao.Trip;
 import com.hypertrack.trips.ShareableTrip;
 
 import org.junit.Test;
@@ -12,9 +14,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
 public class TripModelTest {
+    private static final String TAG = "TripModelTest";
+
     @Test
     public void itShouldCreateShareMessageWithoutEtaIfNoRemainingDurationWasSet() {
         ShareableTrip shareableTrip =
@@ -25,6 +31,8 @@ public class TripModelTest {
         String expected = "Track my live location here https://trck.at/someSubUrl";
 
         String got = model.getShareableMessage();
+        Log.d(TAG, "Got message " + got);
+
         assertEquals(expected, got);
     }
 
@@ -38,7 +46,36 @@ public class TripModelTest {
         Pattern pattern = Pattern.compile("Will be there by \\d\\d?:\\d\\d\\w{2}. Track my live location here https://trck.at/someSubUrl");
 
         String got = model.getShareableMessage();
+        Log.d(TAG, "Got message " + got);
+
         Matcher matcher = pattern.matcher(got);
         assertTrue(matcher.matches());
     }
+
+    @Test
+    public void itShouldCreateShareMessageWithEtaIfRemainingDurationWasReceivedOnUpdate() {
+        String someRandomStuff = "dead-beef-1234";
+        ShareableTrip shareableTrip =
+                new ShareableTrip("https://trck.at/someSubUrl", "", someRandomStuff, null);
+
+        TripModel model = TripModel.fromShareableTrip(shareableTrip);
+
+        Trip trip = mock(Trip.class);
+        Trip.Estimate estimate = mock(Trip.Estimate.class);
+        Trip.Route route = mock(Trip.Route.class);
+        when(route.getRemainingDuration()).thenReturn(42);
+        when(estimate.getRoute()).thenReturn(route);
+        when(trip.getEstimate()).thenReturn(estimate);
+        when(trip.getTripId()).thenReturn(someRandomStuff);
+
+        model.update(trip);
+
+        Pattern pattern = Pattern.compile("Will be there by \\d\\d?:\\d\\d\\w{2}. Track my live location here https://trck.at/someSubUrl");
+
+        String got = model.getShareableMessage();
+        Log.d(TAG, "Got message " + got);
+        Matcher matcher = pattern.matcher(got);
+        assertTrue(matcher.matches());
+    }
+
 }
